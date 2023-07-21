864232688c01a831fae5af8e191d6d5b43a0d5ea
caongao999 <caongao999@gmail.com>
2023-07-21 08:11:10 +0700

    codeinvscode1

---
+++ main.js
@@ -0,0 +1,15 @@
+//alert('hello javascript')//đây là hàm thông báo ra cửa sổ đang mở//
+/*var fullname = 'nguyen_minh_duy'; //đây là hàm gán biến, nhớ sau dấu bằng là string thì có dấu kép, là number thì k
+alert(fullname);*/  //đây là in hàm ra;
+//dấu chấm phẩy có hoặc ko, code vẫn chạy nhưng mà nên có, cùng dòng ko có dấu chấm phẩy ko chạy đc
+//cách comment nhiều dòng sau đây
+/*
+đây là cmt nhiều dòng
+cú pháp
+như đó. hay sử dụng để vô hiệu quả nhiều dòng code
+*/
+//tiếp theo là 1 số hàm có sẵn đơn giản; built-in function//
+setTimeout(function() {
+    alert('hết thời gian')
+    }, 3000);
+    
